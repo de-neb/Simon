822cbdc49e96443bbbff84b69cf4c6e3ba2f4b87
denksy <77825462+denksy@users.noreply.github.com>
2021-03-07 14:45:38 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+const BUTTONCOLORS = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = true;
+let level = 0;
+let userChosenColor;
+
+/* game title screen */
+$("#to-play").on("click", function () {
+  $(".close-t").addClass("top");
+  $(".close-b").addClass("bottom");
+  setTimeout(() => {
+    $("#game-cont").removeClass("hide-game");
+  }, 1000);
+  setTimeout(() => {
+    $(".close-t").removeClass("top");
+    $(".close-b").removeClass("bottom");
+  }, 3000);
+  pressKeyToStart();
+});
+
+
+function pressKeyToStart(){
+  $(document).on("keypress", function (key) {
+    if (started) {
+      started = false;
+      nextSequence();
+      clickingButton();
+    }
+  });
+}
+
+/* user clicks a color */
+function clickingButton() {
+  $(".btn").click(function () {
+    userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length - 1);
+  });
+}
+
+/* produces random color and add in the sequence */
+function nextSequence() {
+  userClickedPattern = [];
+
+  let randomNum = Math.floor(Math.random() * 4);
+  let randomChosenColor = BUTTONCOLORS[randomNum];
+  gamePattern.push(randomChosenColor);
+
+  /* adds flash effect in the random color */
+  $(`#${randomChosenColor}`).addClass("flash");
+  setTimeout(() => $(`#${randomChosenColor}`).removeClass("flash"), 100);
+  playSound(randomChosenColor);
+
+  level++;
+  $("#level-title").text(`Level ${level}`);
+}
+
+function playSound(color) {
+  let audio = new Audio(`sounds/${color}.mp3`);
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $(`#${currentColor}`).addClass("pressed");
+  setTimeout(() => $(`#${currentColor}`).removeClass("pressed"), 200);
+}
+
+function checkAnswer(lastIndex) {
+  let rightColor = gamePattern[lastIndex];
+
+  if (rightColor === userClickedPattern[lastIndex]) {
+    console.log("success"); //for checking
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(nextSequence, 1000);
+    }
+  } else {
+    /* for checking rightColor */
+    try {
+      $(".game-over").removeClass("dim");
+      $("#right-color span")
+        .text(`${rightColor.toUpperCase()}`)
+        .addClass(`text-color-${rightColor}`);
+
+      playSound("wrong");
+      $("#play-again").click(function () {
+        $(".close-t").addClass("top");
+        $(".close-b").addClass("bottom");
+        setTimeout(() => {
+          $("#title-card").addClass("title-card");
+        }, 1000);
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      });
+    } catch {
+      console.log(`${rightColor} is undefined`)
+      console.log(`User pattern length: ${userClickedPattern.length}`)
+    }
+  }
+  console.log(gamePattern, userClickedPattern);
+  console.log(rightColor);
+}
